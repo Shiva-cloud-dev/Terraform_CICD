5c526773eb78f4c1c5d62d58352de5f32df41549
Shiva-cloud-dev <angatashiva916@gmail.com>
2024-10-25 20:12:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0182f373e66f89c85"
+    ami = "ami-06b21ccaeff8cd686"
     instance_type = "t2.micro"
     tags = {
       Name = "dev-ec3"
